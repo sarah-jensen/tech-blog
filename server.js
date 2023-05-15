579b6ae798290fa77099db29e34a9d29534fb54f
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-05-14 20:37:58 -0700

    update server.js file and add handlebars views

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
+const path = require('path');
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils');
 
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+const sessionConfig = {
+  secret: process.env.SECRET,
   resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+  saveUninitialized: false,
 };
+
 app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static())
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
